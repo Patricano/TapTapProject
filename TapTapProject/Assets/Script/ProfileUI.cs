fb051ce1bd73e7d5b863a213abf141931903730a
Patricano <41410441+Patricano@users.noreply.github.com>
2020-10-06 22:03:23 +1100

    ProfileUI and Scene seletion UI
    Profile UI leads to Scene UI where user can select scene and will then open vuforia AR camera

---
+++ TapTapProject/Assets/Script/ProfileUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ProfileUI : MonoBehaviour
+{
+    public GameObject playerName, age, profileUI, Scenes;
+
+    public string PlayerName, Age;
+    public void StartButton()
+    {
+        PlayerName = playerName.GetComponent<InputField>().text;
+        int dropdown_index;
+        dropdown_index = age.GetComponent<Dropdown>().value;
+        Age = age.GetComponent<Dropdown>().options[dropdown_index].text;
+
+        if (PlayerName != "" && Age != "")
+        {
+            profileUI.SetActive(false);
+            Scenes.SetActive(true);
+            Scenes.SendMessage("addNewPlayer", PlayerName);
+        }
+        else
+        {
+            Debug.Log("Input field empty!!");
+        }
+
+
+    }
+
+    public void CLoseAR()
+    {
+        Debug.Log("Quit!!!");
+        Application.Quit();
+    }
+
+}
---
+++ TapTapProject/Assets/Script/ScenesUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScenesUI : MonoBehaviour
+{
+    public GameObject profileUI, scenesUI, arCamera;
+    public Text playerName;
+
+    private void Start()
+    {
+      
+    }
+    public void Back()
+    {
+        profileUI.SetActive(true);
+        scenesUI.SetActive(false);
+    }
+
+    public void addNewPlayer(string name)
+    {
+        playerName.text = name;
+    }
+
+    public void jungleButton()
+    {
+        scenesUI.SetActive(false);
+    }
+}
